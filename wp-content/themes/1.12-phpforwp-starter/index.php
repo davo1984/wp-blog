71adcf43f74dd20d84a8ac0a6783e1a195ce8160
David Oliver <davooliver1964@gmail.com>
2020-08-06 20:16:41 -0400

    starter & child theme exercises

--- wp-content/themes/1.12-phpforwp-starter/index.php
+++ wp-content/themes/1.12-phpforwp-starter/index.php
@@ -3,16 +3,37 @@
     <div id="content">
 
       <!-- Static Front Page -->
+      <?php if ( is_front_page() 
+        && !is_home() ) : ?>
+
+        <h1>Static Front Page</h1>
+
+      <?php endif; ?>
 
       <!-- Blog Home -->
+        <?php if ( is_home() ) : ?>
+            <h2>Blog</h2>
+        <?php endif; ?>
 
       <!-- Page (Not Front Page) -->
+      <?php if ( is_page() && !is_front_page() ): ?>
+        <h3>Page - not front page</h3>
+      <?php endif; ?>
 
       <!-- Single Post -->
+        <?php if ( is_single() ): ?>
+            <h2>Single Post</h2>
+        <?php endif; ?>
 
       <!-- Single Attachment (Media) -->
+        <?php if ( is_attachment() ): ?>
+            <h2>Attachment</h2>
+        <?php endif; ?>
 
       <!-- Category Archive -->
+        <?php if ( is_category_archive() ): ?>
+            <h2>Category Archive</h2>
+        <?php endif; ?>
 
       <!-- Tag Archive -->
 
